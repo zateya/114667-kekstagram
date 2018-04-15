8efd7a9d854e6b7cdfd68ce9f8aa14438e2a5088
Olga Koval <ok@telemark-it.ru>
2018-04-15 22:18:18 +0300

    Добавляет открытие окон и логику применения фильтров

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var SCALE_MAX = 100;
+
 var picturesTestData = {
   minIndex: 1,
   maxIndex: 25,
@@ -68,16 +71,60 @@ var avatarData = {
   maxIndex: 6
 };
 
-var pictureTemplate = document.querySelector('#picture').content;
+var filterNameToRange = {
+  chrome: {
+    min: 0,
+    max: 1
+  },
+  sepia: {
+    min: 0,
+    max: 1
+  },
+  marvin: {
+    min: 0,
+    max: 100
+  },
+  phobos: {
+    min: 0,
+    max: 3
+  },
+  heat: {
+    min: 1,
+    max: 3
+  }
+};
+
+var pictureTemplate = document.querySelector('#picture').content.querySelector('a');
 var picturesList = document.querySelector('.pictures');
 var bigPicture = document.querySelector('.big-picture');
+var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
 var socialCommentCount = document.querySelector('.social__comment-count');
 var socialCommentLoader = document.querySelector('.social__comment-loadmore');
 var picturesFragment = document.createDocumentFragment();
 
+var uploadInput = document.querySelector('#upload-file');
+var uploadOverlay = document.querySelector('.img-upload__overlay');
+var uploadClose = uploadOverlay.querySelector('.img-upload__cancel');
+var uploadImage = document.querySelector('.img-upload__preview');
+
+var pinScale = uploadOverlay.querySelector('.scale__pin');
+var scaleInput = uploadOverlay.querySelector('.scale__value');
+var effectFilters = document.querySelectorAll('.effects__radio');
+var activeFilter = uploadOverlay.querySelector('.effects__radio[checked]');
+
+var isEscEvent = function (evt, action) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    action();
+  }
+};
+
+var getRandomInt = function (num) {
+  return Math.round(Math.random() * num);
+};
+
 // Получение случайного элемента массива
 var getRandomArrayElement = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr[getRandomInt(arr.length - 1)];
 };
 
 // Получение случайного значения из диапазона
@@ -109,6 +156,10 @@ var renderPicture = function (picture) {
   pictureElement.querySelector('.picture__img').src = picture.url;
   pictureElement.querySelector('.picture__stat--likes').textContent = picture.likes;
   pictureElement.querySelector('.picture__stat--comments').textContent = picture.comments.length;
+
+  pictureElement.addEventListener('click', function () {
+    openBigPicture(picture);
+  });
 
   return pictureElement;
 };
@@ -151,15 +202,94 @@ var renderComments = function (comments) {
 };
 
 // Наполнение данными окна с увеличенной фотографией
-var renderBigPicture = function (num) {
-  var pictureData = shuffledData[num];
-
+var renderBigPicture = function (pictureData) {
   bigPicture.querySelector('.big-picture__img > img').src = pictureData.url;
   bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
   bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length;
 
   renderComments(pictureData.comments);
 };
+
+var onBigPictureKeyDown = function (evt) {
+  isEscEvent(evt, closeBigPicture);
+};
+
+var openBigPicture = function (pictureData) {
+  renderBigPicture(pictureData);
+  bigPicture.classList.remove('hidden');
+  document.addEventListener('keydown', onBigPictureKeyDown);
+};
+
+var closeBigPicture = function () {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onBigPictureKeyDown);
+};
+
+bigPictureClose.addEventListener('click', function () {
+  closeBigPicture();
+});
+
+var onUploadKeydown = function (evt) {
+  isEscEvent(evt, closeUpload);
+};
+
+var openUpload = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onUploadKeydown);
+};
+
+var closeUpload = function () {
+  uploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onUploadKeydown);
+  uploadInput.value = '';
+};
+
+uploadInput.addEventListener('change', function () {
+  openUpload();
+});
+
+uploadClose.addEventListener('click', function () {
+  closeUpload();
+});
+
+var getEffectValue = function (effectName, rangeValue) {
+  return (filterNameToRange[effectName].max - filterNameToRange[effectName].min) / SCALE_MAX * rangeValue + filterNameToRange[effectName].min;
+};
+
+var getFilterStyles = function (effectName, effectValue) {
+  var effectToFilterStyles = {
+    chrome: 'filter: grayscale(' + effectValue + ');',
+    sepia: 'filter: sepia(' + effectValue + ');',
+    marvin: 'filter: invert(' + effectValue + '%);',
+    phobos: 'filter: blur(' + effectValue + 'px);',
+    heat: 'filter: brightness(' + effectValue + ');'
+  };
+
+  return effectToFilterStyles[effectName];
+};
+
+var setImageStyle = function () {
+  var filterName = activeFilter.value;
+  var filterValue = getEffectValue(activeFilter.value, scaleInput.value);
+  uploadImage.style = getFilterStyles(filterName, filterValue);
+};
+
+var setFiltersEvents = function () {
+  [].forEach.call(effectFilters, function (effectFilter) {
+    effectFilter.addEventListener('click', function (evt) {
+      if (evt.target.value === 'none') {
+        uploadImage.style = '';
+      } else {
+        activeFilter = evt.target;
+        setImageStyle();
+      }
+    });
+  });
+};
+
+pinScale.addEventListener('mouseup', function () {
+  setImageStyle();
+});
 
 // получение данных со списком фотографий
 var data = picturesTestData.getPictures();
@@ -170,14 +300,11 @@ var shuffledData = shuffleArray(data.slice());
 // вывод фотографий на странице
 renderPictures(shuffledData);
 
-// показ окна с увеличенной фотографией
-bigPicture.classList.remove('hidden');
-
-// отрисовка окна с первой фотографией
-renderBigPicture(0);
-
 // Скрытие количества комментариев в окне с увеличенной фотографией
 socialCommentCount.classList.add('visually-hidden');
 
 // Скрытие загрузчика комментариев в окне с увеличенной фотографией
 socialCommentLoader.classList.add('visually-hidden');
+
+// Добавляет события на фильтры
+setFiltersEvents();
